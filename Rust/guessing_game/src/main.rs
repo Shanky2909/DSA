71905c81c00bbdd139283ea99edd4357f121d33f
Shashank Shekhar <shashekh9@WKMZTA770D1D.global.publicisgroupe.net>
2024-01-03 21:43:10 +0530

    Chapter-2 Hands On_Guessing Game Done

---
+++ Rust/guessing_game/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the Number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+   loop{ println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to Read Line");
+
+    let guess:u32 = match guess.trim().parse() {
+        Ok(num) =>num,
+        Err(_) => continue,
+    };
+
+    println!("you guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small!"),
+        Ordering::Greater => println!("Too bif!"),
+        Ordering::Equal =>{ println!("You Win"); break;},
+    }
+}
+}
